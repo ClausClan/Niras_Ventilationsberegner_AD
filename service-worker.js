218c0ccf810948aa9a9f6a7ba0e3f71bf3248a9f
ClausClan <160112433+ClausClan@users.noreply.github.com>
2026-04-05 22:13:49 +0200

    Versions update 14.5Beta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ventcalc-adv-v6'; // Versionsnummer opdateret
+const CACHE_NAME = 'ventcalc-adv-v7'; // Versionsnummer opdateret
 
 // Basis-URL for GitHub Pages. 
 const BASE_URL = '/Niras_Ventilationsberegner_AD';
